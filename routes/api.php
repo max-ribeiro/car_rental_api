5d6f01ca84df6872e767e010f6b9ff90bc81656d
max-ribeiro <max_ribeiro@live.com>
2023-10-06 23:43:18 -0300

    feat: setup of API routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,10 @@
 <?php
 
+use App\Http\Controllers\BrandController;
+use App\Http\Controllers\CarController;
+use App\Http\Controllers\CarModelController;
+use App\Http\Controllers\ClientController;
+use App\Http\Controllers\RentalController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +22,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResource('brand', BrandController::class);
+Route::apiResource('car', CarController::class);
+Route::apiResource('model', CarModelController::class);
+Route::apiResource('client', ClientController::class);
+Route::apiResource('Rental', RentalController::class);
