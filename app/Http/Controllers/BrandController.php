190862dce3d059a38bf3061ba39f026a4d06b1e6
max-ribeiro <max_ribeiro@live.com>
2023-10-07 00:21:05 -0300

    feat: first delete routes setup

--- app/Http/Controllers/BrandController.php
+++ app/Http/Controllers/BrandController.php
@@ -66,5 +66,7 @@ class BrandController extends Controller
     public function destroy(Brand $brand)
     {
         //
+        $brand->delete();
+        return ['message' => 'deleted'];
     }
 }
